580beba84ab174a5cc9198f124ea2a406305dd87
oliveiradniel <kadadniel@gmail.com>
2025-03-10 20:15:05 -0300

    style: add tailwindcss configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
+import './styles/index.css';
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
