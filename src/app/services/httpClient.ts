b8de591d7a663c30fbe9d2e457a266f14dc91863
oliveiradniel <kadadniel@gmail.com>
2025-03-17 20:39:49 -0300

    feat: add an interceptor to inject the authorization header

--- src/app/services/httpClient.ts
+++ src/app/services/httpClient.ts
@@ -2,8 +2,18 @@ import axios from 'axios';
 
 import { env } from '../../config/env';
 
-const { API_URL } = env;
+const { API_URL, ACCESS_TOKEN_KEY } = env;
 
 export const httpClient = axios.create({
   baseURL: API_URL,
+});
+
+httpClient.interceptors.request.use((config) => {
+  const accessToken = localStorage.getItem(ACCESS_TOKEN_KEY);
+
+  if (accessToken) {
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+
+  return config;
 });
