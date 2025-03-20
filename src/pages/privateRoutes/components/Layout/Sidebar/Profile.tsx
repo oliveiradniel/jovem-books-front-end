56b774f9086e5781aac872062df34adca39db2cb
oliveiradniel <kadadniel@gmail.com>
2025-03-20 19:30:16 -0300

    feat: add options to profile component in side navigation

--- src/pages/privateRoutes/components/Layout/Sidebar/Profile.tsx
+++ src/pages/privateRoutes/components/Layout/Sidebar/Profile.tsx
@@ -8,6 +8,8 @@ import { env } from '../../../../../config/env.ts';
 
 import { FaCircleUser } from 'react-icons/fa6';
 import { IoMdArrowDropdown, IoMdArrowDropup } from 'react-icons/io';
+import { PiGearSixFill } from 'react-icons/pi';
+import { TbLogout2 } from 'react-icons/tb';
 
 interface ProfileProps {
   isExpanded: boolean;
@@ -15,6 +17,9 @@ interface ProfileProps {
 
 export default function Profile({ isExpanded }: ProfileProps) {
   const [hoverOnProfile, setHoverOnProfile] = useState(false);
+
+  const [areTheProfileOptionsOpen, setAreTheProfileOptionsOpen] =
+    useState(false);
 
   const { user } = useAuth();
 
@@ -29,43 +34,76 @@ export default function Profile({ isExpanded }: ProfileProps) {
     animatedElementRef: animatedUsernameRef,
   } = useAnimatedUnmount<HTMLHeadingElement>(isExpanded);
 
-  return (
-    <button
-      type="button"
-      onMouseOver={() => setHoverOnProfile(true)}
-      onMouseOut={() => setHoverOnProfile(false)}
-      className={`hover:bg-navy-blue bottom-0 m-1 flex justify-between rounded-sm px-5 py-2 transition-colors duration-300 ease-in-out hover:cursor-pointer ${!isExpanded && 'justify-center gap-2'}`}
-    >
-      <div className="flex items-center gap-2">
-        {user?.imagePath ? (
-          <img
-            src={`${env.API_URL}/uploads/users/${user.imagePath}`}
-            alt="Foto de Perfil"
-            className="h-7 w-7 rounded-full"
-          />
-        ) : (
-          <FaCircleUser
-            className={`text-mate-gray h-6 w-6 transition-colors duration-300 ease-in-out ${hoverOnProfile && 'text-snow-white!'}`}
-          />
-        )}
-        {shouldRenderUsername && (
-          <h1
-            ref={animatedUsernameRef}
-            className={`animate-fade-in-500 font-quicksand text-snow-white-op-70 whitespace-nowrap transition-colors duration-300 ease-in-out ${hoverOnProfile && 'text-snow-white!'} ${!isExpanded && 'animate-fade-out-100'}`}
-          >
-            {shortenedUsername}
-          </h1>
-        )}
-      </div>
+  const {
+    shouldRender: shouldRenderOptions,
+    animatedElementRef: animatedOptionsRef,
+  } = useAnimatedUnmount<HTMLDivElement>(areTheProfileOptionsOpen);
 
-      {shouldRenderUsername && (
+  function handleVisibilityOfProfileOptionsToggle() {
+    setAreTheProfileOptionsOpen((prevState) => !prevState);
+  }
+
+  return (
+    <>
+      {shouldRenderOptions && (
         <div
-          className={`animate-fade-in-500 text-snow-white-op-70 transition-colors duration-300 ease-in-out ${hoverOnProfile && 'text-snow-white!'} ${!isExpanded && 'animate-fade-out-100'}`}
+          ref={animatedOptionsRef}
+          className={`animate-move-in-top-300 absolute bottom-14 left-0 w-[220px] p-1 ${!areTheProfileOptionsOpen && 'animate-return-to-bottom-200'} ${!isExpanded && 'w-[220px]!'}`}
         >
-          <IoMdArrowDropup />
-          <IoMdArrowDropdown />
+          <div className="bg-navy-blue flex h-full flex-col gap-1 rounded-lg p-1">
+            <div className="text-mate-gray hover:bg-blue-black flex items-center gap-2 rounded-lg px-5 py-2 transition-colors duration-300 ease-in-out hover:cursor-pointer">
+              <PiGearSixFill className="text-lg" />
+              <span className="text-sm">Perfil</span>
+            </div>
+
+            <div className="bg-blue-black-op-80 h-[0.1px] w-full"></div>
+
+            <div className="text-mate-gray hover:bg-blue-black flex items-center gap-2 rounded-lg px-5 py-2 transition-colors duration-300 ease-in-out hover:cursor-pointer">
+              <TbLogout2 className="text-lg" />
+              <span className="text-sm">Sair</span>
+            </div>
+          </div>
         </div>
       )}
-    </button>
+
+      <button
+        type="button"
+        onClick={handleVisibilityOfProfileOptionsToggle}
+        onMouseOver={() => setHoverOnProfile(true)}
+        onMouseOut={() => setHoverOnProfile(false)}
+        className={`hover:bg-navy-blue bottom-0 m-1 flex justify-between rounded-sm px-5 py-2 transition-colors duration-300 ease-in-out hover:cursor-pointer ${!isExpanded && 'justify-center gap-2'}`}
+      >
+        <div className="flex items-center gap-2">
+          {user?.imagePath ? (
+            <img
+              src={`${env.API_URL}/uploads/users/${user.imagePath}`}
+              alt="Foto de Perfil"
+              className="h-7 w-7 rounded-full"
+            />
+          ) : (
+            <FaCircleUser
+              className={`text-mate-gray h-6 w-6 transition-colors duration-300 ease-in-out ${hoverOnProfile && 'text-snow-white!'}`}
+            />
+          )}
+          {shouldRenderUsername && (
+            <h1
+              ref={animatedUsernameRef}
+              className={`animate-fade-in-500 font-quicksand text-snow-white-op-70 whitespace-nowrap transition-colors duration-300 ease-in-out ${hoverOnProfile && 'text-snow-white!'} ${!isExpanded && 'animate-fade-out-100'}`}
+            >
+              {shortenedUsername}
+            </h1>
+          )}
+        </div>
+
+        {shouldRenderUsername && (
+          <div
+            className={`animate-fade-in-500 text-snow-white-op-70 transition-colors duration-300 ease-in-out ${hoverOnProfile && 'text-snow-white!'} ${!isExpanded && 'animate-fade-out-100'}`}
+          >
+            <IoMdArrowDropup />
+            <IoMdArrowDropdown />
+          </div>
+        )}
+      </button>
+    </>
   );
 }
