0ea99c2b388d502a2be53f276f5cfafe9e0ed1a4
oliveiradniel <kadadniel@gmail.com>
2025-03-23 17:15:50 -0300

    style: customize the scroll bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
+import tailwindScrollbar from 'tailwind-scrollbar';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js}'],
+  plugins: [tailwindScrollbar],
 };
