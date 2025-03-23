416f353498f225ec57612bd95fae4ab25e6ec13d
oliveiradniel <kadadniel@gmail.com>
2025-03-23 16:09:54 -0300

    feat: animate the entry and exit of options in select on my books page

--- src/pages/privateRoutes/MyBooks/components/Select/Options.tsx
+++ src/pages/privateRoutes/MyBooks/components/Select/Options.tsx
@@ -1,3 +1,4 @@
+import useAnimatedUnmount from '../../../../../app/hooks/useAnimatedUnmount.ts';
 import { Page } from '../../@types/Page';
 import Button from './Button';
 
@@ -12,14 +13,18 @@ export default function Options({
   isOptionsVisible,
   onSelect,
 }: OptionsProps) {
-  if (!isOptionsVisible) {
+  const { shouldRender, animatedElementRef } =
+    useAnimatedUnmount<HTMLDivElement>(isOptionsVisible);
+
+  if (!shouldRender) {
     return null;
   }
 
   return (
     <div
+      ref={animatedElementRef}
       id="options"
-      className="font-roboto bg-navy-blue absolute mt-12 flex w-[170px] flex-col gap-1 rounded-lg p-1"
+      className={`font-roboto bg-navy-blue animate-move-in-bottom-d300-y30 absolute mt-12 flex w-[170px] flex-col gap-1 rounded-lg p-1 ${!isOptionsVisible && 'animate-return-to-top-d200-y50'}`}
     >
       <Button
         numberOfItems={23}
