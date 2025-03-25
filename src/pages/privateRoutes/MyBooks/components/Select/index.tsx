c8c2e408df069f661e13668385642df874288393
oliveiradniel <kadadniel@gmail.com>
2025-03-25 04:52:29 -0300

    style: overlay navigation menu

--- src/pages/privateRoutes/MyBooks/components/Select/index.tsx
+++ src/pages/privateRoutes/MyBooks/components/Select/index.tsx
@@ -16,12 +16,12 @@ export default function Select({
   onTogglingOfOptionsVisibility,
 }: SelectProps) {
   return (
-    <div className="flex w-[140px]">
+    <div className="z-1 flex w-[140px]">
       <button
         id="select"
         type="button"
         onClick={onTogglingOfOptionsVisibility}
-        className={`bg-navy-blue text-sky-blue font-quicksand hover:bg-navy-blue-op-80 w-full rounded-lg p-2 text-sm transition-colors duration-300 ease-in-out hover:cursor-pointer ${isOptionsVisible && 'hover:bg-navy-blue!'}`}
+        className={`bg-navy-blue text-mate-gray font-quicksand hover:bg-navy-blue-op-80 w-full rounded-lg p-2 text-sm font-semibold transition-colors duration-300 ease-in-out hover:cursor-pointer ${isOptionsVisible && 'hover:bg-navy-blue!'}`}
       >
         {MY_BOOKS_PAGES[page]}
       </button>
