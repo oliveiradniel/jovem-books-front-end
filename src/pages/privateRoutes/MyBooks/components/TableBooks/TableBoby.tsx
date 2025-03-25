4add295412a377c861a4d6a279ed869d657c529a
oliveiradniel <kadadniel@gmail.com>
2025-03-24 22:01:30 -0300

    feat: add the possibility to click on a row in the books table

--- src/pages/privateRoutes/MyBooks/components/TableBooks/TableBoby.tsx
+++ src/pages/privateRoutes/MyBooks/components/TableBooks/TableBoby.tsx
@@ -12,11 +12,21 @@ interface TableBodyProps {
 }
 
 export default function TableBody({ books }: TableBodyProps) {
+  function handleClickOnBookData(bookId: string) {
+    console.log(bookId);
+  }
+
   return (
     <tbody className="text-mate-gray font-quicksand">
       {books.map((book) => (
-        <tr className="border-navy-blue border-b">
-          <td className="px-2 py-2 text-center">
+        <tr
+          role="button"
+          tabIndex={0}
+          key={book.id}
+          onClick={() => handleClickOnBookData(String(book.id))}
+          className="border-navy-blue hover:bg-navy-blue-op-40 border-b hover:cursor-pointer"
+        >
+          <td className="rounded-tl-lg rounded-bl-lg px-2 py-2 text-center">
             {truncateString(book.title, 10)}
           </td>
           <td className="px-2 py-2 text-center">
@@ -25,9 +35,9 @@ export default function TableBody({ books }: TableBodyProps) {
           <td className="px-2 py-2 text-center">
             {BOOK_LITERARY_GENRE[book.genreLiterary]}
           </td>
-          <td className="px-2 py-2 text-center">
+          <td className="rounded-tr-lg rounded-br-lg px-2 py-2 text-center">
             <span
-              className={`flex items-center justify-center rounded-sm p-1 font-semibold ${book.status === 'READING' && 'bg-ocean-blue'} ${book.status === 'READING' && 'bg-blue-black'} ${book.status === 'FINISHED' && 'bg-royal-blue'} ${book.status === 'ON_HOLD' && 'border-navy-blue border'} ${book.status === 'NOT_READING' && 'bg-blue-black'}`}
+              className={`text-snow-white flex items-center justify-center rounded-sm p-1 font-semibold ${book.status === 'READING' && 'bg-ocean-blue'} ${book.status === 'READING' && 'bg-blue-black'} ${book.status === 'FINISHED' && 'bg-royal-blue'} ${book.status === 'ON_HOLD' && 'border-navy-blue border'} ${book.status === 'NOT_READING' && 'bg-blue-black'}`}
             >
               {READING_STATUS[book.status]}
             </span>
