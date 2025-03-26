37a357a3609c48c0c93285b5af0d9129d516244b
oliveiradniel <kadadniel@gmail.com>
2025-03-26 05:40:10 -0300

    feat: implement the possibility of starting to read the book

--- src/pages/privateRoutes/Book.tsx
+++ src/pages/privateRoutes/Book.tsx
@@ -7,19 +7,26 @@ import { books } from '../../assets/mocks/books';
 import { Book as IBook } from '../../@types/Book';
 
 import { GoArrowLeft } from 'react-icons/go';
+import { GrInProgress } from 'react-icons/gr';
 
 export default function Book() {
-  const [book, setBook] = useState<IBook>();
+  const [book, setBook] = useState<IBook>({} as IBook);
 
   const { id } = useParams();
 
   const navigate = useNavigate();
 
+  function handleWithTheBeginninOfReading() {
+    const newBook = { ...book, status: 'READING' };
+
+    setBook(newBook as IBook);
+  }
+
   useEffect(() => {
     async function getBookById() {
       const dataBook = books.find((book) => book.id === Number(id));
 
-      setBook(dataBook);
+      setBook(dataBook as IBook);
     }
 
     getBookById();
@@ -52,9 +59,16 @@ export default function Book() {
           <div>
             <button
               type="button"
-              className="bg-sky-blue-op-80 text-snow-white font-roboto hover:bg-sky-blue-op-60 mt-10 rounded-lg px-3 py-2 text-sm font-semibold transition-colors duration-300 ease-in-out hover:cursor-pointer"
+              disabled={book.status !== 'NOT_READING'}
+              onClick={handleWithTheBeginninOfReading}
+              className="hover:bg-navy-blue-op-80 border-navy-blue text-snow-white font-roboto bg-navy-blue disabled:bg-navy-blue-op-40 disabled:border-navy-blue-op-80 hover:border-navy-blue-op-80 mt-10 flex h-10 w-[140px] items-center justify-center rounded-lg border px-3 py-2 text-sm font-semibold transition-colors duration-300 ease-in-out hover:cursor-pointer disabled:cursor-default"
             >
-              INICIAR LEITURA
+              {book.status === 'NOT_READING' && 'INICIAR LEITURA'}
+              {book.status === 'READING' && (
+                <p className="flex items-center justify-center gap-2">
+                  <GrInProgress /> EM LEITURA
+                </p>
+              )}
             </button>
           </div>
         </div>
@@ -67,6 +81,27 @@ export default function Book() {
           />
         </div>
       </div>
+
+      {book.status === 'READING' && (
+        <div className="bg-navy-blue-op-80 text-snow-white-op-70 font-quicksand animate-fade-in-500 mt-5 rounded-lg px-4 py-2">
+          <p className="flex gap-2">
+            Leitura iniciada em:{' '}
+            <span className="text-light-gray font-semibold">
+              24 de dezembro de 2024
+            </span>
+          </p>
+          <p className="flex gap-2">
+            Total de páginas:{' '}
+            <span className="text-light-gray font-semibold">
+              <p> 302</p>
+            </span>
+          </p>
+          <p className="flex gap-2">
+            <p>Página atual: </p>{' '}
+            <span className="text-light-gray font-semibold">225</span>
+          </p>
+        </div>
+      )}
     </div>
   );
 }
