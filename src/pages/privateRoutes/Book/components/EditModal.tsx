d21b908a0d627ccb34a6b5a7dab6c243e7dcaf97
oliveiradniel <kadadniel@gmail.com>
2025-03-28 05:28:19 -0300

    feat: add buttons to enable incrementing and decrementing da página atual

--- src/pages/privateRoutes/Book/components/EditModal.tsx
+++ src/pages/privateRoutes/Book/components/EditModal.tsx
@@ -4,6 +4,7 @@ import { useAuth } from '../../../../app/hooks/useAuth.ts';
 
 import Modal from './Modal.tsx';
 import Input from './Input.tsx';
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 interface EditModal {
   currentPage: number | null;
@@ -24,7 +25,8 @@ export default function EditModal({
 
   const [numberOfPage, setNumberOfPage] = useState<number | null>(null);
 
-  const buttonDisabled = Number(numberOfPage) === 0 || !numberOfPage;
+  const buttonDisabled =
+    Number(numberOfPage) === 0 || !numberOfPage || currentPage === numberOfPage;
 
   function handlePageNumberChange(event: ChangeEvent<HTMLInputElement>) {
     const { value } = event.target;
@@ -52,6 +54,15 @@ export default function EditModal({
     handleClose();
   }
 
+  function handlePageIncrement() {
+    setNumberOfPage((prevState) => (prevState ?? 0) + 1);
+  }
+
+  function handlePageDecrement() {
+    if (numberOfPage === 0) return;
+    setNumberOfPage((prevState) => (prevState ?? 0) - 1);
+  }
+
   useEffect(() => {
     const value = currentPage ? currentPage : null;
 
@@ -67,13 +78,32 @@ export default function EditModal({
       onClose={onClose}
       onConfirm={handleConfirm}
     >
-      <Input
-        autoFocus
-        type="text"
-        placeholder="Número da página atual"
-        value={numberOfPage ?? ''}
-        onChange={handlePageNumberChange}
-      />
+      <div className="flex items-center gap-2">
+        <Input
+          autoFocus
+          type="text"
+          placeholder="Página atual"
+          value={numberOfPage ?? ''}
+          onChange={handlePageNumberChange}
+        />
+
+        <button
+          type="button"
+          onClick={handlePageIncrement}
+          className="bg-navy-blue-2 text-snow-white hover:bg-navy-blue flex h-8 w-8 items-center justify-center rounded-lg transition-colors duration-300 ease-in-out hover:cursor-pointer"
+        >
+          <AiOutlinePlus size={14} />
+        </button>
+
+        <button
+          type="button"
+          disabled={numberOfPage === 0}
+          onClick={handlePageDecrement}
+          className="bg-navy-blue-2 text-snow-white hover:bg-navy-blue disabled:bg-navy-blue-op-80 flex h-8 w-8 items-center justify-center rounded-lg transition-colors duration-300 ease-in-out hover:cursor-pointer disabled:cursor-default"
+        >
+          <AiOutlineMinus size={14} />
+        </button>
+      </div>
     </Modal>
   );
 }
--- src/pages/privateRoutes/Book/components/Input.tsx
+++ src/pages/privateRoutes/Book/components/Input.tsx
@@ -1,7 +1,7 @@
 export default function Input({ ...props }) {
   return (
     <input
-      className="bg-navy-blue-2 focus:border-snow-white-op-70 font-quicksand text-light-gray border-navy-blue-2 mt-2 h-8 appearance-none rounded-[4px] border px-2 text-sm transition-colors duration-300 ease-in-out outline-none"
+      className="bg-navy-blue focus:border-sky-blue-op-60 font-quicksand text-light-gray border-navy-blue-2 h-8 appearance-none rounded-[4px] border px-2 text-sm transition-colors duration-300 ease-in-out outline-none"
       {...props}
     />
   );
