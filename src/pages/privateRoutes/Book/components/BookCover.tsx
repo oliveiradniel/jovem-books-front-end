776b03c6b50675fca4ff26d65458eabc0752f0ab
oliveiradniel <kadadniel@gmail.com>
2025-03-30 15:17:11 -0300

    feat: add conditional rendering if there is no book cover

--- src/pages/privateRoutes/Book/components/BookCover.tsx
+++ src/pages/privateRoutes/Book/components/BookCover.tsx
@@ -1,3 +1,5 @@
+import { GiBookCover } from 'react-icons/gi';
+
 interface BookCoverProps {
   imagePath: string | null;
 }
@@ -5,7 +7,11 @@ interface BookCoverProps {
 export default function BookCover({ imagePath }: BookCoverProps) {
   return (
     <div className="hidden max-w-[240px] min-w-[240px] items-center justify-center lg:flex">
-      <img src={imagePath} alt="Capa do Livro" className="h-auto w-[100%]" />
+      {imagePath ? (
+        <img src={imagePath} alt="Capa do Livro" className="h-auto w-[100%]" />
+      ) : (
+        <GiBookCover size={160} color="#03a9f4 " />
+      )}
     </div>
   );
 }
