cedba149a129f26044bb8b22277e569ceff2467b
oliveiradniel <kadadniel@gmail.com>
2025-05-28 21:51:04 -0300

    style: temporarily hide 'new collection' card visually

--- src/pages/privateRoutes/Dashboard/index.tsx
+++ src/pages/privateRoutes/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import AddCategoryIcon from './components/AddCategoryIcon';
+// import AddCategoryIcon from './components/AddCategoryIcon';
 import AddBookIcon from './components/AddBookIcon';
 import Card from './components/Card';
 
@@ -6,20 +6,21 @@ export default function Dashboard() {
   return (
     <div>
       <p className="font-roboto text-light-gray">
-        Aqui você é capaz de adicionar{' '}
-        <span className="text-sky-blue">novos livros</span> e{' '}
-        <span className="text-sky-blue">novas categorias</span> para separar
-        melhor seus livros.
+        Aqui você é capaz de adicionar
+        <span className="text-sky-blue"> novos livros.</span>
+        {/* e{' '}
+        <span className="text-sky-blue">novas categorias</span>  */}
+        {/* para separar melhor seus livros. */}
       </p>
 
       <div className="mt-10 flex flex-col items-center gap-4 p-4 sm:flex-row">
         <Card to="/new-book" Icon={AddBookIcon} buttonLabel="Novo livro" />
-        <Card
+        {/* <Card
           to="/new-collection"
           Icon={AddCategoryIcon}
           buttonLabel="Nova coleção"
           disabled
-        />
+        /> */}
       </div>
     </div>
   );
