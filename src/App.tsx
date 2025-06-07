632e7412146702bedbf8778df2c4a60a176b248e
oliveiradniel <kadadniel@gmail.com>
2025-06-07 06:44:41 -0300

    refactor: separate queryClient settings into another file

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,13 @@
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 import { AuthProvider } from './app/contexts/AuthProvider';
 
 import { Slide, ToastContainer } from 'react-toastify';
 
-import Router from './Router';
+import { queryClient } from './config/queryClient';
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 5000,
-      refetchOnWindowFocus: false,
-      retry: false,
-      gcTime: 10 * 60 * 1000, // 10 minutes
-    },
-  },
-});
+import Router from './Router';
 
 function App() {
   return (
