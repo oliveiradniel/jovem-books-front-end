1a264b49a8e8fdf8a83fd77c6897ef0d968f1802
oliveiradniel <kadadniel@gmail.com>
2025-07-07 18:54:02 -0300

    change: change the way responsiveness is applied to the book card coming from Google Books

--- src/pages/privateRoutes/GoogleBooks/components/Card.tsx
+++ src/pages/privateRoutes/GoogleBooks/components/Card.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import { truncateString } from '../../../../utils/truncateString';
 
 import { FaBookBookmark } from 'react-icons/fa6';
+import { MdLibraryAdd } from 'react-icons/md';
 
 import { IGoogleBookAPI } from '../../../../@types/Book';
 
@@ -16,32 +17,19 @@ export default function Card({ book }: CardProps) {
 
   const [isFocused, setIsFocused] = useState(false);
 
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
   const numberOfAuthors = book.authors?.length;
   const hasAuthors = numberOfAuthors > 0;
-  const moreThanOneAuthor = numberOfAuthors - 1 > 0;
-  const author =
-    hasAuthors && truncateString(book.authors[0], windowWidth < 900 ? 10 : 14);
+  const moreThanOneAuthor = numberOfAuthors > 1;
+  const author = hasAuthors && truncateString(book.authors[0], 14);
   const displayAuthor = author
     ? `${author} ${moreThanOneAuthor ? `+${numberOfAuthors}` : ''}`
     : 'Sem informações';
-
-  useEffect(() => {
-    function handleResize() {
-      setWindowWidth(window.innerWidth);
-    }
-
-    window.addEventListener('resize', handleResize);
-
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
 
   return (
     <div
       onMouseEnter={() => setIsFocused(true)}
       onMouseLeave={() => setIsFocused(false)}
-      className="bg-navy-blue/70 animate-fade-in relative flex h-[clamp(6vw,32vw,250px)] w-[clamp(5vw,24vw,180px)] flex-col items-center justify-evenly gap-2 shadow-lg transition-all duration-300 ease-in-out hover:cursor-pointer sm:px-4 sm:py-2 lg:h-[250px] lg:w-[180px]"
+      className="bg-navy-blue/70 animate-fade-in relative flex h-[200px] w-[140px] flex-col items-center justify-evenly gap-2 px-2 shadow-lg transition-all duration-300 ease-in-out hover:cursor-pointer sm:px-4 sm:py-2 md:h-[250px] md:w-[190px] lg:h-[250px] lg:w-[180px]"
     >
       {isFocused && (
         <div
@@ -52,12 +40,10 @@ export default function Card({ book }: CardProps) {
               state: { book, cameFromGoogleBooks: true },
             })
           }
-          className="text-sky-blue animate-fade-in absolute top-0 left-0 flex h-full w-full items-center justify-center"
+          className="animate-fade-in absolute top-0 left-0 flex h-full w-full items-center justify-center"
         >
           <div className="relative z-2 flex h-full w-full items-center justify-center bg-black/60 backdrop-blur-[4px]">
-            <p className="font-roboto text-snow-white absolute z-3 mx-3 rounded-sm text-center text-[clamp(10px,2vw,16px)] font-semibold">
-              CLIQUE PARA ADICIONAR
-            </p>
+            <MdLibraryAdd className="text-snow-white" size={60} />
           </div>
           {book.imagePath && (
             <img
@@ -69,11 +55,8 @@ export default function Card({ book }: CardProps) {
         </div>
       )}
 
-      <p className="text-snow-white/70 font-quicksand flex min-h-6 items-center text-center text-[clamp(0.6rem,0.8rem,1rem)] sm:min-h-13 sm:text-[clamp(0.8rem,1rem,1rem)]">
-        {truncateString(
-          book.title,
-          windowWidth < 720 ? 14 : windowWidth < 900 ? 20 : 28
-        )}
+      <p className="text-snow-white/70 font-quicksand flex min-h-6 items-center text-center text-sm md:text-[16px] xl:text-[18px]">
+        {truncateString(book.title, 20)}
       </p>
 
       {book.imagePath ? (
@@ -81,21 +64,16 @@ export default function Card({ book }: CardProps) {
           <img
             src={book.imagePath}
             alt="Capa do Livro"
-            className={`rounded-full object-cover transition-all duration-300 ease-in-out ${windowWidth < 720 ? 'h-13 w-13' : windowWidth < 900 ? 'h-16 w-16' : 'h-24 w-24'}`}
+            className="h-[68px] w-[68px] rounded-full object-cover transition-all duration-300 ease-in-out"
           />
         </div>
       ) : (
-        <div
-          className={`flex items-center justify-center ${windowWidth < 720 ? 'h-13 w-13' : windowWidth < 900 ? 'h-16 w-16' : 'h-24 w-24'}`}
-        >
-          <FaBookBookmark
-            size={windowWidth < 720 ? 30 : windowWidth < 900 ? 40 : 60}
-            color="#ffffff50"
-          />
+        <div className="flex items-center justify-center">
+          <FaBookBookmark size={60} className="text-snow-white/60" />
         </div>
       )}
 
-      <p className="text-sky-blue font-quicksand text- flex items-center text-center text-[clamp(0.4rem,0.8rem,1rem)] sm:h-12 sm:min-h-10 sm:text-[clamp(0.8rem,1rem,1rem)]">
+      <p className="text-sky-blue font-quicksand text- flex items-center text-center text-sm md:text-[16px] xl:text-[18px]">
         {displayAuthor}
       </p>
     </div>
